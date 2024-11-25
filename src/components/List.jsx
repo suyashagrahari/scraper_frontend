9ab33b2090692c831a6362e0a9bb7e61e63092a2
suyashagrahari <suyashagrahari73@gmail.com>
2024-11-25 18:32:01 +0530

    fixing search box

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -215,7 +215,7 @@ function CompanyList() {
         <div className="w-full md:w-[50%] relative">
           <input
             type="text"
-            placeholder="Enter domain name"
+            placeholder="Enter Company Name"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             className="w-full md:max-w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent text-sm md:text-base"
@@ -223,7 +223,7 @@ function CompanyList() {
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4 md:w-5 md:h-5" />
         </div>
         <button className="w-full md:w-auto px-6 py-2 bg-purple-200 hover:bg-purple-700 hover:text-white text-purple-700 rounded-lg transition duration-200 ease-in-out text-xs md:text-sm font-medium">
-          Fetch & Save Details
+          Search
         </button>
       </div>
 
